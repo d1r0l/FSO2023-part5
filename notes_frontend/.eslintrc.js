b33020864d1f79f6a1df26c688aea9fbb9c24543
d1r0l <xd1r0lx@gmail.com>
2023-07-05 20:45:52 +0400

    added eslint & coursedata update

---
+++ notes_frontend/.eslintrc.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  settings: {
+    'react': {
+      'version': 'detect'
+    }
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0
+  }
+}
--- notes_frontend/src/App.js
+++ notes_frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Note from './componets/Note'
-import noteService from "./services/notes";
+import noteService from './services/notes'
+import loginService from './services/login'
 
 const Notification = ({ message }) => {
   if (message === null) {
@@ -33,6 +34,10 @@ const App = () => {
   const [ newNote, setNewNote ] = useState('A new note...')
   const [ showAll, setShowAll ] = useState(true)
   const [ errorMessage, setErrorMessage ] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState(null)
 
   const toggleImportanceOf = (id) => {
     const note = notes.find(note => note.id === id)
@@ -64,8 +69,8 @@ const App = () => {
   }, [])
 
   const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important)
+    ? notes
+    : notes.filter(note => note.important)
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
@@ -73,7 +78,7 @@ const App = () => {
 
   const addNote = (event) => {
     event.preventDefault()
-      const newObject = {
+    const newObject = {
       id: notes.length + 1,
       content: newNote,
       important: Math.random() < 0.5
@@ -87,18 +92,76 @@ const App = () => {
       })
   }
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>
+  )
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      {!user && loginForm()}
+      {user && <div>
+        <p>{user.name} logged in</p>
+        {noteForm()}
+      </div>
+      }
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
       </div>
       <ul>
-        {notesToShow.map((note) => 
-          <Note 
+        {notesToShow.map((note) =>
+          <Note
             key={note.id}
             note={note}
             toggleImportance={() => toggleImportanceOf(note.id)}
