6ce76a7b816204ebef9798edbef0ee17d42ab8f5
d1r0l <xd1r0lx@gmail.com>
2023-07-15 01:24:41 +0400

    backend get status response fix

--- bloglist_backend/controllers/blogs.js
+++ bloglist_backend/controllers/blogs.js
@@ -4,7 +4,7 @@ const Blog = require('../models/blog')
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
     .populate('user', { username: 1, name: 1, id: 1 })
-  response.json(blogs)
+  response.status(200).json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
--- bloglist_backend/controllers/users.js
+++ bloglist_backend/controllers/users.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt')
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({})
     .populate('blogs', { title: 1, author: 1, url: 1 })
-  response.json(users)
+  response.status(200).json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
