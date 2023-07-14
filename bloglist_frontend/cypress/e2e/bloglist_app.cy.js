63dc720d2a1fb42b69e5b43b039bd44017dedc89
d1r0l <xd1r0lx@gmail.com>
2023-07-14 19:31:15 +0400

    completed ex 5.18

--- bloglist_frontend/cypress/e2e/bloglist_app.cy.js
+++ bloglist_frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,6 +1,11 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.request('POST', `http://localhost:3001/api/testing/reset`)
+    cy.request('POST', `http://localhost:3001/api/users`,{
+      username: 'tester',
+      password: 'p455w0rd',
+      name: 'Cypress Will'
+    })
     cy.visit('http://localhost:3000')
   })
 
@@ -13,5 +18,22 @@ describe('Blog app', function() {
     cy.get('#input-username')
     cy.get('#input-password')
     cy.get('#button-login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#input-username').type('tester')
+      cy.get('#input-password').type('p455w0rd')
+      cy.get('#button-login').click()
+      cy.contains('login successful').should('have.css', 'border-color', 'rgb(0, 128, 0)')
+      cy.contains('Cypress Will logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#input-username').type('username')
+      cy.get('#input-password').type('password')
+      cy.get('#button-login').click()
+      cy.contains('wrong credentials').should('have.css', 'border-color', 'rgb(255, 0, 0)')
+    })
   })
 })
